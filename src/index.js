04ae59960ab7f73f4806059c0b5cacd12f0ec2b1
StepanKarpyak <stepan_karpyak@ukr.net>
2022-05-19 21:11:20 +0300

    cтворення+

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { App } from 'components/App';
 import './index.css';
+import { store, persistor } from './redux/store';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
